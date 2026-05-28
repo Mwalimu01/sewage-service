efb29ed73a1d9b3aea1196591eb33293e30b052b
Mwalimu01 <ruchagaana@gmail.com>
2026-05-28 18:37:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE_NAME = 'sewage-service-v1';
+const urlsToCache = [
+    '/sewage-service/',
+    '/sewage-service/index.html',
+    '/sewage-service/manifest.json',
+    '/sewage-service/icon-192.png',
+    '/sewage-service/icon-512.png'
+];
 
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
